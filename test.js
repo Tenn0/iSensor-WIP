9b24404d85efbc27edce6e4972f7bbeb3925787c
tenn0 <rougerom9@gmail.com>
2020-08-12 04:23:10 +0200

    try with fetch and ajax on productive system, using token as var

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 
-function getValueFromHS() {
+function getValueFromHSajax() {
     var request = new XMLHttpRequest();
     request.open("GET", "https://192.268.2.100:8123/api/states/sensor.outside_temperature")
     request.setRequestHeader("Authentification", "Bearer [token]");
@@ -7,17 +7,34 @@ function getValueFromHS() {
     request.send();
     console.log("hi!!");
     request.addEventListener('load', function(event) {
-        if (request.status >= 200 && request.status < 300) {
-           console.log(request.responseText);
+        if (request.status = 200) {
+           console.log("response: " + request.responseText);
         } else {
-           console.warn(request.statusText, request.responseText);
+           console.warn("responee" + request.statusText, request.responseText);
         }
      });
+     if( request.status = 200){
+        console.log("OK");
+     }
      alert("hi");
-     console.log(request.response);
+     console.log("prob response: " + request.response);
 }
 
+function getValueFromHSfetch(){
+   var request = fetch(url="http://localhost:8123/api/states/sensor.outside_temperature",{
+      mode: 'cors',
+      headers:{
+         "Authentification": "tenn0 eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiIwYzdhZTVjMjdjMDU0Nzg5YjExMzMzM2E4NWExMGY3YiIsImlhdCI6MTU5NzE5NDMyNSwiZXhwIjoxOTEyNTU0MzI1fQ.KRir-HwzFOE807Wk6j4QXE9hAqvYutGYkEZuJ9Ph9Z4",
+         "Content-Type": "application/json"
+      }
+   });
+   console.log(request.response);
 
+
+
+}
    var test = document.getElementById('test1')
-   test.addEventListener('click', getValueFromHS, true);1
+   test.addEventListener('click', getValueFromHSfetch, true);1
+
+
    
